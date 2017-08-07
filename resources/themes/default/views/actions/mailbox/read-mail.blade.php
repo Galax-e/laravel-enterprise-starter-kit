c12f23d29d7f71ff09352cf96beb6c5e623c8edf
Chukwuma Nwaugha <c.nwaugha@gmail.com>
2017-08-07 22:22:29 +0100

    memo modal

--- resources/themes/default/views/actions/mailbox/read-mail.blade.php
+++ resources/themes/default/views/actions/mailbox/read-mail.blade.php
@@ -112,7 +112,7 @@
                                <li>
                                    <span class="mailbox-attachment-icon"><i class="fa fa-file-pdf-o"></i></span>
                                    <div class="mailbox-attachment-info">
-                                           <i class="fa fa-paperclip"></i> <a href="../attachment_file/'.$attachment->name.'" style="color: #000000 ;" target="_blank"> '.$attachment->original_name.'</a><br/> <!-- </a> -->
+                                           <i class="fa fa-paperclip"></i> <a href="../attachment_file/'.$attachment->name.'" style="color: #000000 ;" data-toggle="modal" data-target="#FileModal"> '.$attachment->original_name.'</a><br/> <!-- </a> -->
                                            <span class="mailbox-attachment-size">
                                                '.$attachment->created_at.'
 <a href="#" data-toggle="modal" data-target="#FileModal" class="btn btn-default btn-xs pull-right"><i class="fa fa-cloud-download"></i></a>
