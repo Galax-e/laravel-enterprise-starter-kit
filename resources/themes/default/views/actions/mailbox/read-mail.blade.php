2379fb507dd14fedd9c464db46d70ef35a9f3995
Chukwuma Nwaugha <c.nwaugha@gmail.com>
2017-08-07 22:04:44 +0100

    attachment on red mail

--- resources/themes/default/views/actions/mailbox/read-mail.blade.php
+++ resources/themes/default/views/actions/mailbox/read-mail.blade.php
@@ -67,7 +67,7 @@
                                <li>
                                    <span class="mailbox-attachment-icon"><i class="fa fa-file-image-o"></i></span>
                                    <div class="mailbox-attachment-info">
-                                           <i class="fa fa-paperclip"></i> <a href="../attachment_file/'.$attachment->name.'" style="color: #000000 ;" target="_blank"> '.$attachment->original_name.'</a><br/> <!-- </a> -->
+                                           <i class="fa fa-paperclip"></i> <a href="../attachment_file/'.$attachment->name.'" style="color: #000000 ;" data-toggle="modal" data-target="#FileModal"> '.$attachment->original_name.'</a><br/> <!-- </a> -->
                                            <span class="mailbox-attachment-size">
                                                '.$attachment->created_at.'
 
